f20d847a0897f511025e516f8524b6e27b8ecf26
raccoonmonk <raccoonmonk@gmail.com>
2014-11-29 22:05:22 +0300

    +overloaded recognize function to parse memory block

--- sphinxwrapper.cpp
+++ sphinxwrapper.cpp
@@ -23,7 +23,7 @@ std::string SphinxWrapper::recognize(const std::string & filename) {
   if(fh == NULL)
     return result;
 
-  rv = ps_decode_raw(ps, fh, "goforward", -1);
+  rv = ps_decode_raw(ps, fh, NULL, -1);
   if(rv < 0)
     return result;
   hyp = ps_get_hyp(ps, &score, &uttid);
@@ -32,6 +32,34 @@ std::string SphinxWrapper::recognize(const std::string & filename) {
 
   result = std::string(hyp);
   fclose(fh);
+  ps_free(ps);
+  return result;
+}
+
+std::string SphinxWrapper::recognize(const char * buf, int size) {
+  std::string result("ERROR. Something went wrong.");
+  config = cmd_ln_init(NULL, ps_args(), TRUE,
+           "-hmm", MODELDIR,
+           "-lm", MODELDIR "/turtle.DMP",
+           "-dict", MODELDIR "/turtle.dic",
+           NULL);
+  if (config == NULL)
+      return result;
+  ps = ps_init(config);
+  if (ps == NULL)
+    return result;
+
+  rv = ps_start_utt(ps, NULL);
+    if (rv < 0)
+      return result;
+  rv = ps_process_raw(ps, (const int16 *)buf, size/2, FALSE, FALSE);
+  rv = ps_end_utt(ps);
+  if (rv < 0)
+    return result;
+  hyp = ps_get_hyp(ps, &score, &uttid);
+  if (hyp == NULL)
+    return result;
+  result = std::string(hyp);
   ps_free(ps);
   return result;
 }
--- sphinxwrapper.h
+++ sphinxwrapper.h
@@ -9,20 +9,27 @@ public:
   SphinxWrapper();
 
   /*!
-   * \brief recognize speech in the audio file
+   * \brief recognize speech from an audio file
    * \param filename name of the audio file
    * \return std::string with recognized text
    */
   std::string recognize(const std::string & filename);
 
+  /*!
+   * \brief recognize speech from a block of memory
+   * \param buf pointer to a data block
+   * \param size size of dlock in bytes
+   * \return std::string with recognized text
+   */
+  std::string recognize(const char * buf, int size);
+
 private:
-    ps_decoder_t *ps;
-    cmd_ln_t *config;
-    FILE *fh;
-    char const *hyp, *uttid;
-          int16 buf[512];
-    int rv;
-    int32 score;
+  ps_decoder_t *ps;
+  cmd_ln_t *config;
+  FILE *fh;
+  char const *hyp, *uttid;
+  int rv;
+  int32 score;
 };
 
 #endif // SPHINXWRAPPER_H
